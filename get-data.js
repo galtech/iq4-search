c9d82815265b7e5870f0fe5710af780c86c47054
Peter Heylin <peter@peterheylin.com>
2018-12-03 23:05:31 +0000

    20181203 added fetch method

--- get-data.js
+++ get-data.js
@@ -41,9 +41,9 @@ const app = new Vue({
   computed: {
     filtered(){
       if(this.query.length >= this.startAt){
-        return this.jobs.filter(job => {
-          if(jobs.hasOwnProperty(this.filterKey)){
-            return job[this.filterKey]
+        return this.jobs.filter(item => {
+          if(job.hasOwnProperty(this.filterKey)){
+            return item[this.filterKey]
             .toLowerCase()
             .indexOf(this.query.toLowerCase()) > -1
           } else {
